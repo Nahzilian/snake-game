59e6e7dd24054eca0333e159bac6d425764ae07a
Nahzilian <nahz3112@gmail.com>
2021-03-23 00:24:15 -0400

    Added auto movement

--- snake-game/src/Components/Board/Board.jsx
+++ snake-game/src/Components/Board/Board.jsx
@@ -1,35 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SinglyLinkedList } from '../../utils/linkedlist';
 import { getCoordsInDirection } from '../../utils/directions';
+import { useInterval } from '../../utils/useIntervals';
 import './Board.css';
 
 export default function Board() {
-    const BOARDSIZE = 10;
+    const BOARDSIZE = 15;
     const Direction = {
         UP: 'UP',
         DOWN: 'DOWN',
         LEFT: 'LEFT',
         RIGHT: 'RIGHT'
     }
+
+    const getCentralPoint = size => {
+        let row = Math.floor(size / 2), col = Math.floor(size / 2);
+        let cell = board[row][col]
+        return { row, col, cell };
+    }
+
     // Setting a 10 x 10 board
     const [board, setBoard] = useState(boardGeneration(BOARDSIZE));
-    const [snakeCells, setSnakeCells] = useState(new Set([44])); // This is the body of the snake
-    const [snake, setSnake] = useState(new SinglyLinkedList(44));
+    const [snake, setSnake] = useState(new SinglyLinkedList(getCentralPoint(BOARDSIZE)));
+    const [snakeCells, setSnakeCells] = useState(new Set([snake.head.value.cell])); // This is the body of the snake
     const [direction, setDirection] = useState(Direction.RIGHT);
+    const [move, setMove] = useState(100);
+
+    
     const moveSnake = () => {
         const snakePos = {
             row: snake.head.value.row,
             col: snake.head.value.col
         }
-
-        const nextHeadCoords = getCoordsInDirection(snakePos, direction);
+        const nextPos = getCoordsInDirection(snakePos, direction);
+        const nextHeadCell = board[nextPos.row][nextPos.col]
+        const newHead = { row: nextPos.row, col: nextPos.col, cell: nextHeadCell}
+        setSnake(new SinglyLinkedList(newHead))
+        setSnakeCells(new Set([nextHeadCell]));
     }
+    
+    useInterval(() => {
+        moveSnake();
+        //setMove(prev => prev - 1);
+        //setSnakeCells(prev => new Set([move]));
+        //console.log(snakeCells);
+    }, [1000])
+
     return (
         <div className="board">
             {board.map((row, rowIndex) => (
                 <div key={rowIndex} className="row">
                     {row.map((cell, cellIndex) => (
-                        <div key={cellIndex} className={`cell ${snakeCells.has(cell) ? 'snake-cell' : ''}`}>{cell}</div>
+                        <div key={cellIndex} className={`cell ${snakeCells.has(cell) ? 'snake-cell' : ''}`}>
+                            {cell}
+                        </div>
                         ))}
                 </div>
             ))}
@@ -45,3 +69,5 @@ const boardGeneration = size => {
     }
     return board;
 }
+
+
--- snake-game/src/utils/linkedlist.js
+++ snake-game/src/utils/linkedlist.js
@@ -13,3 +13,4 @@ export class SinglyLinkedList {
         this.tail = node;
     }
 }
+
